84666497716d94d8ce950c3effa814d5b9d80053
Vlad Balas <balash_vlad@yahoo.com>
2023-03-19 21:11:27 +0000

    Connected to Server and DB on Railway

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://facerecognitionbrain-server.up.railway.app/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -73,7 +73,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://facerecognitionbrain-server.up.railway.app/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
